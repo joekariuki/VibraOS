3816e80b09d47ab097e3f2e9622148c7a4f538a8
Joe Kariuki <jknguru6@gmail.com>
2019-10-05 17:55:00 -0400

    Closes #16; Add PCB prototype

--- distrib/os/pcb.js
+++ distrib/os/pcb.js
@@ -1,24 +1,33 @@
 var TSOS;
 (function (TSOS) {
-    var Pcb = /** @class */ (function () {
-        function Pcb(pid, segment, priority, PC, Acc, Xreg, Yreg, Zflag, isExecuting) {
+    var PCB = /** @class */ (function () {
+        function PCB(PID, priority, PC, Acc, Xreg, base, limit, isExecuting, pcbProgram, Yreg, Zflag, state) {
+            if (PID === void 0) { PID = _PID; }
+            if (priority === void 0) { priority = _PRIORITY; }
             if (PC === void 0) { PC = 0; }
             if (Acc === void 0) { Acc = 0; }
             if (Xreg === void 0) { Xreg = 0; }
+            if (base === void 0) { base = _BASE; }
+            if (limit === void 0) { limit = _ProgramSize - 1; }
+            if (isExecuting === void 0) { isExecuting = false; }
+            if (pcbProgram === void 0) { pcbProgram = ""; }
             if (Yreg === void 0) { Yreg = 0; }
             if (Zflag === void 0) { Zflag = 0; }
-            if (isExecuting === void 0) { isExecuting = false; }
-            this.pid = pid;
-            this.segment = segment;
+            if (state === void 0) { state = PS_NEW; }
+            this.PID = PID;
             this.priority = priority;
             this.PC = PC;
             this.Acc = Acc;
             this.Xreg = Xreg;
+            this.base = base;
+            this.limit = limit;
+            this.isExecuting = isExecuting;
+            this.pcbProgram = pcbProgram;
             this.Yreg = Yreg;
             this.Zflag = Zflag;
-            this.isExecuting = isExecuting;
+            this.state = state;
         }
-        return Pcb;
+        return PCB;
     }());
-    TSOS.Pcb = Pcb;
+    TSOS.PCB = PCB;
 })(TSOS || (TSOS = {}));
--- source/os/pcb.ts
+++ source/os/pcb.ts
@@ -1,17 +1,20 @@
 
 module TSOS {
 
-    export class Pcb {
+    export class PCB {
     
-        constructor(public pid: number,
-                    public segment: number,
-                    public priority: number,
+        constructor(public PID: number = _PID,
+                    public priority: number = _PRIORITY,
                     public PC: number = 0,
                     public Acc: number = 0,
                     public Xreg: number = 0,
+                    public base: number = _BASE,
+                    public limit: number = _ProgramSize - 1,
+                    public isExecuting: boolean = false,
+                    public pcbProgram :string = "",
                     public Yreg: number = 0,
                     public Zflag: number = 0,
-                    public isExecuting: boolean = false) {
-            }
+                    public state: number = PS_NEW) {
         }
     }
+}
